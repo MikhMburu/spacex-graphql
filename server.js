89145d791ab940e8e05e870077a06f4dbbc4e961
Michael N <mikhmburu@live.com>
2021-05-20 22:18:02 +0300

    Finished client

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // Import libraries
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
+const path = require("path");
 // Import files
 const schema = require("./schema");
 // Define variables and functions
 const app = express();
 const port = process.env.PORT || 5000;
 // Define Middleware
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -14,5 +17,10 @@ app.use(
     graphiql: true,
   })
 );
+
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 app.listen(port, () => console.log(`App running on port ${port}`));
